7c3afaef1abfde9d4eef484776be463475aa2455
Matthias Skov Bryde <matthiasskovbryde@gmail.com>
2024-10-28 16:58:12 +0100

    Mapping AddressRequest to Address

--- lib/PlanSphere.Core/Features/Addresses/Profiles/AddressProfile.cs
+++ lib/PlanSphere.Core/Features/Addresses/Profiles/AddressProfile.cs
@@ -1,13 +1,19 @@
-﻿using Domain.Entities;
-using AutoMapper;
+﻿using AutoMapper;
+using Domain.Entities;
 using PlanSphere.Core.Features.Addresses.Requests;
 
-namespace PlanSphere.Core.Features.Profiles;
+namespace PlanSphere.Core.Features.Addresses.Profiles;
 
 public class AddressProfile : Profile
 {
     public AddressProfile()
     {
-        CreateMap<AddressRequest, Address>();
+        CreateMap<AddressRequest, Address>()
+            .ForMember(dest => dest.CountryId, opt => opt.MapFrom(src => src.CountryId))
+            .ForMember(dest => dest.HouseNumber, opt => opt.MapFrom(src => src.HouseNumber))
+            .ForMember(dest => dest.PostalCode, opt => opt.MapFrom(src => src.PostalCode))
+            .ForMember(dest => dest.StreetName, opt => opt.MapFrom(src => src.StreetName))
+            .ForMember(dest => dest.Door, opt => opt.MapFrom(src => src.Door))
+            .ForMember(dest => dest.Floor, opt => opt.MapFrom(src => src.Floor));
     }
 }
