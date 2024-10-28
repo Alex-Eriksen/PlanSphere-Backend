b3ef0e46ae4f3aede7961a9ed8e451755357efd9
Alexander Vestergaard Eriksen <alexanderv.eriksen@gmail.com>
2024-10-28 16:41:20 +0100

    Added configuration for keycloak

---
+++ lib/PlanSphere.Core/Extensions/APIExtensions/AuthorizationExtensions.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+
+namespace PlanSphere.Core.Extensions.APIExtensions;
+
+public static class AuthorizationExtensions
+{
+    public static IServiceCollection AddSwaggerGenWithAuth(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddSwaggerGen(o =>
+        {
+            o.AddXMLComments();
+            
+            o.AddSecurityDefinition("KeyCloak",
+                new OpenApiSecurityScheme
+                {
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.OAuth2,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    Flows = new OpenApiOAuthFlows
+                    {
+                        Implicit = new OpenApiOAuthFlow
+                        {
+                            AuthorizationUrl = new Uri(configuration["KeyCloakConfiguration:AuthorizationUrl"]!),
+                            TokenUrl = new Uri(configuration["KeyCloakConfiguration:TokenUrl"]!)
+                        }
+                    }
+                });
+
+            o.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Id = "KeyCloak",
+                            Type = ReferenceType.SecurityScheme
+                        }
+                    },
+                    new List<string>()
+                    {
+                        "openid"
+                    }
+                }
+            });
+
+        });
+
+        return services;
+    }
+}
--- src/PlanSphere.SystemApi/Controllers/RoleRepository.cs
+++ src/PlanSphere.SystemApi/Controllers/RoleRepository.cs
@@ -1,10 +1,12 @@
 ﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using PlanSphere.Core.Features.Roles.Commands.CreateRole;
 using PlanSphere.SystemApi.Controllers.Base;
 
 namespace PlanSphere.SystemApi.Controllers;
 
+[Authorize]
 public class RoleRepository(IMediator mediator) : ApiControllerBase(mediator)
 {
     private readonly IMediator _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
