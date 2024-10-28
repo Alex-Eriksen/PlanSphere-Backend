5431daf02b2bda9c771900eadeefeb42ea38ac8c
Matthias <matthiasskovbryde@gmail.com>
2024-10-28 13:32:13 +0100

    Added organisation delete command

---
+++ lib/PlanSphere.Core/Features/Organisations/Commands/DeleteOrganisation/DeleteOrganisationCommandHandler.cs
@@ -0,0 +1,22 @@
+using MediatR;
+using Microsoft.Extensions.Logging;
+using PlanSphere.Core.Interfaces.Repositories;
+
+namespace PlanSphere.Core.Features.Organisations.Commands.DeleteOrganisation;
+
+public class DeleteOrganisationCommandHandler(
+    IOrganisationRepository organisationRepository,
+    ILogger<DeleteOrganisationCommandHandler> logger
+) : IRequestHandler<DeleteOrganisationCommand>
+{
+    private readonly IOrganisationRepository _organisationRepository = organisationRepository ?? throw new ArgumentNullException(nameof(organisationRepository));
+    private readonly ILogger<DeleteOrganisationCommandHandler> _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+    public async Task Handle(DeleteOrganisationCommand request, CancellationToken cancellationToken)
+    {
+        logger.BeginScope("DeleteOrganisationCommandHandler");
+        
+        _logger.LogInformation("Deleting organisation with id: [{id}]", request.Id);
+        await _organisationRepository.DeleteAsync(request.Id, cancellationToken);
+        _logger.LogInformation("Deleted organisation with id: [{id}]", request.Id);
+    }
+}
---
+++ lib/PlanSphere.Core/Interfaces/Repositories/IOrganisationRepository.cs
@@ -0,0 +1,8 @@
+using Domain.Entities;
+
+namespace PlanSphere.Core.Interfaces.Repositories;
+
+public interface IOrganisationRepository : IRepository<Organisation>
+{
+    
+}
