9dfb9dddda08d402ec0625b092ad3712dfea9f72
Matthias Skov Bryde <matthiasskovbryde@gmail.com>
2024-10-28 16:51:50 +0100

    CreateOrganisation with Command and handler and Address relations

---
+++ src/PlanSphere.SystemApi/Controllers/Organisation/OrganisationController.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using PlanSphere.Core.Features.Organisations.Commands.CreateOrganisation;
+using PlanSphere.SystemApi.Controllers.Base;
+
+namespace PlanSphere.SystemApi.Controllers.Organisation;
+
+public class OrganisationController(IMediator mediator) : ApiControllerBase(mediator)
+{
+    private readonly IMediator _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+
+    [HttpPost(Name = nameof(CreateOrganisationAsync))]
+    public async Task<IActionResult> CreateOrganisationAsync(CreateOrganisationCommand command)
+    {
+        await _mediator.Send(command);
+        return Created();
+    }
+}
