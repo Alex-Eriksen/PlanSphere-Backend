92f0ba826e9cc79f6ba6f6248dd2389f684f64aa
Nicklas F. Osbeck <nicklas@estatetool.dk>
2024-10-28 18:03:07 +0100

    added get job title query with handler and validator

---
+++ lib/PlanSphere.Core/Features/JobTitles/Queries/GetJobTitle/GetJobTitleQuery.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+using PlanSphere.Core.Features.Jobtitles.DTOs;
+
+namespace PlanSphere.Core.Features.JobTitles.Queries.GetJobTitle;
+
+public record GetJobTitleQuery(ulong Id) : IRequest<JobTitleDTO>;
---
+++ lib/PlanSphere.Core/Features/JobTitles/Queries/GetJobTitle/GetJobTitleQueryHandler.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using PlanSphere.Core.Attributes;
+using PlanSphere.Core.Enums;
+using PlanSphere.Core.Features.Jobtitles.DTOs;
+using PlanSphere.Core.Interfaces.Repositories;
+
+namespace PlanSphere.Core.Features.JobTitles.Queries.GetJobTitle;
+
+[HandlerType(HandlerType.SystemApi)]
+public class GetJobTitleQueryHandler(
+    IJobTitleRepository jobTitleRepository,
+    ILogger<GetJobTitleQueryHandler> logger,
+    IMapper mapper
+) : IRequestHandler<GetJobTitleQuery, JobTitleDTO>
+{
+    private readonly IJobTitleRepository _jobTitleRepository = jobTitleRepository ?? throw new ArgumentNullException(nameof(jobTitleRepository));
+    private readonly ILogger<GetJobTitleQueryHandler> _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+    private readonly IMapper _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+
+    public async Task<JobTitleDTO> Handle(GetJobTitleQuery request, CancellationToken cancellationToken)
+    {
+        _logger.BeginScope("Fetching job title with id: [{jobTitleId}].", request.Id);
+        var jobTitle = await _jobTitleRepository.GetByIdAsync(request.Id, cancellationToken);
+        _logger.LogInformation("Fetched job title with id: [{jobTitleId}].", request.Id);
+
+        var mappedJobTitle = _mapper.Map<JobTitleDTO>(jobTitle);
+
+        return mappedJobTitle;
+    }
+}
