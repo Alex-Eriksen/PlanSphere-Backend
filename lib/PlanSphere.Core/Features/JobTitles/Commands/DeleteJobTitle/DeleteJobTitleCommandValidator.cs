f35f284f287c9e8652f7be5a3b92d6e756b233a7
Nicklas F. Osbeck <nicklas@estatetool.dk>
2024-10-28 18:02:51 +0100

    Added delete job title command with handler and validator

---
+++ lib/PlanSphere.Core/Features/JobTitles/Commands/DeleteJobTitle/DeleteJobTitleCommandValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+
+namespace PlanSphere.Core.Features.JobTitles.Commands.DeleteJobTitle;
+
+public class DeleteJobTitleCommandValidator : AbstractValidator<DeleteJobTitleCommand>
+{
+    public DeleteJobTitleCommandValidator()
+    {
+        RuleFor(x => x.Id)
+            .NotNull();
+        
+        RuleFor(x => x.SourceLevel)
+            .NotNull()
+            .IsInEnum();
+
+        RuleFor(x => x.SourceLevelId)
+            .NotNull();
+    }
+}
