ed463bd9f517614cf3730e590a4e0ef3e30c04ab
Matthias <matthiasskovbryde@gmail.com>
2024-10-29 14:56:13 +0100

    GetOrganisation and ListOrganisation almost works

---
+++ lib/PlanSphere.Core/Features/Organisations/Requests/OrganisationRequest.cs
@@ -0,0 +1,13 @@
+using PlanSphere.Core.Features.Roles.Requests;
+
+namespace PlanSphere.Core.Features.Organisations.Requests;
+
+public class OrganisationRequest
+{
+    public string Name { get; set; }
+    public List<ulong> Members { get; set; }
+
+    public List<CompanyRequest> CompanyRequests { get; set; }
+    public List<JobTitleRequest> JobTitleRequests { get; set; }
+    public List<RoleRequest> RoleRequests { get; set; }
+}
