d6352ad2342267a4e2e7afe6c1e0c67f5452a546
Nicklas F. Osbeck <nicklas@estatetool.dk>
2024-10-29 15:08:27 +0100

    Added image validator

---
+++ lib/PlanSphere.Core/Constants/ByteSizeConstants.cs
@@ -0,0 +1,10 @@
+﻿namespace PlanSphere.Core.Constants;
+
+public static class ByteSizeConstants
+{
+    public const long ONE_KILOBYTE = 1_024;
+    public const long ONE_MEGABYTE = ONE_KILOBYTE * 1024;
+    public const long FIVE_MEGABYTES = ONE_MEGABYTE * 5;
+    public const long HUNDRED_MEGABYTES = ONE_MEGABYTE * 100;
+    public const long ONE_GIGABYTE = ONE_MEGABYTE * 1024;
+}
---
+++ lib/PlanSphere.Core/Validation/Validators/ImageValidator.cs
@@ -0,0 +1,31 @@
+﻿using FluentValidation;
+using Microsoft.AspNetCore.Http;
+using PlanSphere.Core.Constants;
+
+namespace PlanSphere.Core.Validation.Validators;
+
+public class ImageValidator : AbstractValidator<IFormFile>
+{
+    public ImageValidator()
+    {
+        RuleFor(x => x)
+            .Must(BeAValidImage)
+            .WithMessage("Invalid file. Only .jpg and .png files are allowed, and the file size must not exceed 5MB.");
+    }
+    
+    private static bool BeAValidImage(IFormFile? file)
+    {
+        if (file == null)
+        {
+            return true;
+        }
+
+        if (file.Length > ByteSizeConstants.FIVE_MEGABYTES)
+        {
+            return false;
+        }
+
+        var ext = Path.GetExtension(file.FileName).ToLowerInvariant();
+        return !string.IsNullOrEmpty(ext) && PermittedImageExtensions.All.Contains(ext);
+    }
+}
