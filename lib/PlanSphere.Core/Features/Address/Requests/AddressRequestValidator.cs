ba1d8640e4a42907b445508bda94d900fef50702
NicklasFjeldsted <nicklasfjeldstedosbeck@gmail.com>
2024-10-30 16:43:48 +0100

    Modified address and it's related objects

---
+++ lib/PlanSphere.Core/Features/Address/Requests/AddressRequestValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+
+namespace PlanSphere.Core.Features.Address.Requests;
+
+public class AddressRequestValidator : AbstractValidator<AddressRequest>
+{
+    public AddressRequestValidator()
+    {
+        RuleFor(x => x.StreetName)
+            .NotNull()
+            .NotEmpty();
+        
+        RuleFor(x => x.HouseNumber)
+            .NotNull()
+            .NotEmpty();
+    }
+}
