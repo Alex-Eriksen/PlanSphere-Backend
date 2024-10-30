9050047f514663d788f203b7481f5448695af41a
NicklasFjeldsted <nicklasfjeldstedosbeck@gmail.com>
2024-10-30 16:46:33 +0100

    Added user repository methods

--- lib/PlanSphere.Infrastructure/Repositories/UserRepository.cs
+++ lib/PlanSphere.Infrastructure/Repositories/UserRepository.cs
@@ -1,22 +1,38 @@
 ﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using PlanSphere.Core.Interfaces.Database;
 using PlanSphere.Core.Interfaces.Repositories;
 using BC = BCrypt.Net.BCrypt;
 
 namespace PlanSphere.Infrastructure.Repositories;
 
-public class UserRepository(IPlanSphereDatabaseContext dbContext) : IUserRepository
+public class UserRepository(IPlanSphereDatabaseContext dbContext, ILogger<UserRepository> logger) : IUserRepository
 {
     private readonly IPlanSphereDatabaseContext _dbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
+    private readonly ILogger<UserRepository> _logger = logger ?? throw new ArgumentNullException(nameof(logger));
     
     public async Task<User> CreateAsync(User request, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        _dbContext.Users.Add(request);
+        await _dbContext.SaveChangesAsync(cancellationToken);
+        return request;
     }
 
-    public Task<User> GetByIdAsync(ulong id, CancellationToken cancellationToken)
+    public async Task<User> GetByIdAsync(ulong id, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        var user = await _dbContext.Users
+            .Include(x => x.Settings)
+            .Include(x => x.Address)
+            .SingleOrDefaultAsync(user => user.Id == id, cancellationToken);
+        
+        if (user == null)
+        {
+            _logger.LogInformation("Could not find user with id: [{userId}]. User doesn't exist!", id);
+            throw new KeyNotFoundException($"Could not find user with id: [{id}]. User doesn't exist!");
+        }
+
+        return user;
     }
 
     public Task<User> UpdateAsync(ulong id, User request, CancellationToken cancellationToken)
@@ -37,5 +53,26 @@ public class UserRepository(IPlanSphereDatabaseContext dbContext) : IUserReposit
     public IQueryable<User> GetQueryable()
     {
         throw new NotImplementedException();
+    }
+
+    public async Task<User> GetByIdentityId(string identityId, CancellationToken cancellationToken)
+    {
+        var user = await _dbContext.Users
+            .Include(x => x.Settings)
+            .Include(x => x.Address)
+            .SingleOrDefaultAsync(user => user.IdentityUserId == identityId, cancellationToken);
+        
+        if (user == null)
+        {
+            _logger.LogInformation("Could not find user with identity id: [{identityId}]. User doesn't exist!", identityId);
+            throw new KeyNotFoundException($"Could not find user with identity id: [{identityId}]. User doesn't exist!");
+        }
+
+        return user;
+    }
+
+    public async Task<bool> IsUserRegisteredAsync(string email, CancellationToken cancellationToken)
+    {
+        return await _dbContext.Users.AnyAsync(user => user.Email == email, cancellationToken);
     }
 }
