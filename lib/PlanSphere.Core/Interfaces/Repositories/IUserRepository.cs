7b44bcbe067060d3b111e7f10ac39c3439b46703
NicklasFjeldsted <nicklasfjeldstedosbeck@gmail.com>
2024-10-30 16:46:33 +0100

    Added user repository methods

--- lib/PlanSphere.Core/Interfaces/Repositories/IUserRepository.cs
+++ lib/PlanSphere.Core/Interfaces/Repositories/IUserRepository.cs
@@ -4,5 +4,6 @@ namespace PlanSphere.Core.Interfaces.Repositories;
 
 public interface IUserRepository : IRepository<User>
 {
-    
+    Task<User> GetByIdentityId(string identityId, CancellationToken cancellationToken);
+    Task<bool> IsUserRegisteredAsync(string email, CancellationToken cancellationToken);
 }
