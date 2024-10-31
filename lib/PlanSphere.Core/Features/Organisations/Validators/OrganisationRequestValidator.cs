b92d10db2e0944e7c471a5ec6ba5c5bead09f046
NicklasFjeldsted <nicklasfjeldstedosbeck@gmail.com>
2024-10-31 13:28:04 +0100

    Removed duplicate address feature and fixed validator error

--- lib/PlanSphere.Core/Features/Organisations/Validators/OrganisationRequestValidator.cs
+++ lib/PlanSphere.Core/Features/Organisations/Validators/OrganisationRequestValidator.cs
@@ -1,4 +1,3 @@
-using Domain.Entities;
 using FluentValidation;
 using PlanSphere.Core.Features.Organisations.Requests;
 
@@ -6,13 +5,10 @@ namespace PlanSphere.Core.Features.Organisations.Validators;
 
 public class OrganisationRequestValidator : AbstractValidator<OrganisationRequest>
 {
-    private readonly OrganisationRequestValidator _validator;
-    public OrganisationRequestValidator(OrganisationRequestValidator validator)
+    public OrganisationRequestValidator()
     {
-        _validator = validator ?? throw new ArgumentNullException(nameof(validator));
-
         RuleFor(x => x.Name).NotNull();
-        RuleFor(x => x.Members).NotNull();
+        RuleFor(x => x.MemberIds).NotNull();
         RuleFor(x => x.CompanyRequests).NotNull();
         RuleFor(x => x.JobTitleRequests).NotNull();
         RuleFor(x => x.RoleRequests).NotNull();
