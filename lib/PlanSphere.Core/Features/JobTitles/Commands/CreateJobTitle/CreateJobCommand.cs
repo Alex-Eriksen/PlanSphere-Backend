f3c673c6a02183de6f9550709cca94f912c612da
NicklasFjeldsted <nicklasfjeldstedosbeck@gmail.com>
2024-11-02 12:32:16 +0100

    Fixed controller parameters

--- lib/PlanSphere.Core/Features/JobTitles/Commands/CreateJobTitle/CreateJobCommand.cs
+++ lib/PlanSphere.Core/Features/JobTitles/Commands/CreateJobTitle/CreateJobCommand.cs
@@ -1,7 +1,15 @@
 ﻿using MediatR;
+using Newtonsoft.Json;
 using PlanSphere.Core.Enums;
 using PlanSphere.Core.Features.JobTitles.Requests;
 
 namespace PlanSphere.Core.Features.JobTitles.Commands.CreateJobTitle;
 
-public record CreateJobTitleCommand(SourceLevel SourceLevel, ulong SourceLevelId, JobTitleRequest Request) : IRequest;
+public record CreateJobTitleCommand(JobTitleRequest Request) : IRequest
+{
+    [JsonIgnore]
+    public ulong SourceLevelId { get; set; }
+    [JsonIgnore]
+    public SourceLevel SourceLevel { get; set; }
+    
+}
