39fef4a6925dbcfaaf6091dfcd38321e9b39654a
NicklasFjeldsted <nicklasfjeldstedosbeck@gmail.com>
2024-11-05 16:50:54 +0100

    Fix spacing

--- src/PlanSphere.SystemApi/Controllers/CompanyController.cs
+++ src/PlanSphere.SystemApi/Controllers/CompanyController.cs
@@ -17,74 +17,75 @@ using PlanSphere.SystemApi.Extensions;
 
 namespace PlanSphere.SystemApi.Controllers;
     
-    [Authorize]
-   public class CompanyController(IMediator mediator) : ApiControllerBase(mediator)
-   {
-       private readonly IMediator _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
-       [TypeFilter(typeof(RoleActionFilter), Arguments = [Right.View])]
-       [HttpGet("{companyId}", Name = nameof(GetCompanyById))]
-       public async Task<IActionResult> GetCompanyById([FromRoute] ulong companyId)
-       {
-           var query = new GetCompanyQuery(companyId);
-           var response = await _mediator.Send(query);
-           return Ok(response);
-       }
-       
-       [HttpGet(Name = nameof(ListCompaniesAsync))]
-       [TypeFilter(typeof(RoleActionFilter), Arguments = [Right.View])]
-       public async Task<IActionResult> ListCompaniesAsync([FromQuery] ListCompaniesQuery query)
-       {
-           query.OrganisationId = Request.HttpContext.User.GetOrganizationId();
-           var response = await _mediator.Send(query);
-           return Ok(response);
-       }
-       
-       [HttpPost(Name = nameof(CreateCompanyAsync))]
-       [TypeFilter(typeof(RoleActionFilter), Arguments = [Right.Edit])]
-       public async Task<IActionResult> CreateCompanyAsync([FromBody] CreateCompanyCommand command)
-       {
-           command.OrganisationId = Request.HttpContext.User.GetOrganizationId();
-           await _mediator.Send(command);
-           return Created();
-       }
-       
-       [HttpPatch("{companyId}", Name = nameof(PatchCompanyAsync))] 
-       [TypeFilter(typeof(RoleActionFilter), Arguments = [Right.Edit])]
-       public async Task<IActionResult> PatchCompanyAsync([FromRoute] ulong companyId, [FromBody] JsonPatchDocument<CompanyUpdateRequest> patchRequest)
-       {
-           var command = new PatchCompanyCommand(patchRequest);
-           command.Id = companyId;
-           await _mediator.Send(command);
-           return Created();
-       }
-       
-       [TypeFilter(typeof(RoleActionFilter), Arguments = [Right.Edit])]
-       [HttpDelete("{companyId}", Name = nameof(DeleteCompanyAsync))] 
-       public async Task<IActionResult> DeleteCompanyAsync([FromRoute] ulong companyId)
-       {
-           var command = new DeleteCompanyCommand(companyId);
-           await _mediator.Send(command);
-           return NoContent();
-       }
-       
-       [HttpPost("{companyId}", Name = nameof(UploadCompanyLogoAsync))] 
-       [TypeFilter(typeof(RoleActionFilter), Arguments = [Right.Administrator])]
-       public async Task<IActionResult> UploadCompanyLogoAsync([FromRoute] ulong companyId, [FromForm] UploadCompanyLogoCommand command)
-       {
-           command.OrganisationId = Request.HttpContext.User.GetOrganizationId();
-           command.CompanyId = companyId;
-           var response = await _mediator.Send(command);
-           return Ok(response);
-       }
-       
-       [HttpGet(Name = nameof(LookUpCompaniesAsync))]
-       [TypeFilter(typeof(RoleActionFilter), Arguments = [Right.View])]
-       public async Task<IActionResult> LookUpCompaniesAsync()
-       {
-           var organisationId = Request.HttpContext.User.GetOrganizationId();
-           var userId = Request.HttpContext.User.GetUserId();
-           var query = new LookUpCompaniesQuery(organisationId, userId);
-           var response = await _mediator.Send(query);
-           return Ok(response);
-       }
+[Authorize]
+public class CompanyController(IMediator mediator) : ApiControllerBase(mediator)
+{
+    private readonly IMediator _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+    
+    [TypeFilter(typeof(RoleActionFilter), Arguments = [Right.View])]
+    [HttpGet("{companyId}", Name = nameof(GetCompanyById))]
+    public async Task<IActionResult> GetCompanyById([FromRoute] ulong companyId)
+    {
+        var query = new GetCompanyQuery(companyId);
+        var response = await _mediator.Send(query);
+        return Ok(response);
+    }
+    
+    [HttpGet(Name = nameof(ListCompaniesAsync))]
+    [TypeFilter(typeof(RoleActionFilter), Arguments = [Right.View])]
+    public async Task<IActionResult> ListCompaniesAsync([FromQuery] ListCompaniesQuery query)
+    {
+        query.OrganisationId = Request.HttpContext.User.GetOrganizationId();
+        var response = await _mediator.Send(query);
+        return Ok(response);
+    }
+    
+    [HttpPost(Name = nameof(CreateCompanyAsync))]
+    [TypeFilter(typeof(RoleActionFilter), Arguments = [Right.Edit])]
+    public async Task<IActionResult> CreateCompanyAsync([FromBody] CreateCompanyCommand command)
+    {
+        command.OrganisationId = Request.HttpContext.User.GetOrganizationId();
+        await _mediator.Send(command);
+        return Created();
+    }
+    
+    [HttpPatch("{companyId}", Name = nameof(PatchCompanyAsync))] 
+    [TypeFilter(typeof(RoleActionFilter), Arguments = [Right.Edit])]
+    public async Task<IActionResult> PatchCompanyAsync([FromRoute] ulong companyId, [FromBody] JsonPatchDocument<CompanyUpdateRequest> patchRequest)
+    {
+        var command = new PatchCompanyCommand(patchRequest);
+        command.Id = companyId;
+        await _mediator.Send(command);
+        return Created();
+    }
+    
+    [TypeFilter(typeof(RoleActionFilter), Arguments = [Right.Edit])]
+    [HttpDelete("{companyId}", Name = nameof(DeleteCompanyAsync))] 
+    public async Task<IActionResult> DeleteCompanyAsync([FromRoute] ulong companyId)
+    {
+        var command = new DeleteCompanyCommand(companyId);
+        await _mediator.Send(command);
+        return NoContent();
+    }
+    
+    [HttpPost("{companyId}", Name = nameof(UploadCompanyLogoAsync))] 
+    [TypeFilter(typeof(RoleActionFilter), Arguments = [Right.Administrator])]
+    public async Task<IActionResult> UploadCompanyLogoAsync([FromRoute] ulong companyId, [FromForm] UploadCompanyLogoCommand command)
+    {
+        command.OrganisationId = Request.HttpContext.User.GetOrganizationId();
+        command.CompanyId = companyId;
+        var response = await _mediator.Send(command);
+        return Ok(response);
+    }
+    
+    [HttpGet(Name = nameof(LookUpCompaniesAsync))]
+    [TypeFilter(typeof(RoleActionFilter), Arguments = [Right.View])]
+    public async Task<IActionResult> LookUpCompaniesAsync()
+    {
+        var organisationId = Request.HttpContext.User.GetOrganizationId();
+        var userId = Request.HttpContext.User.GetUserId();
+        var query = new LookUpCompaniesQuery(organisationId, userId);
+        var response = await _mediator.Send(query);
+        return Ok(response);
+    }
 }
