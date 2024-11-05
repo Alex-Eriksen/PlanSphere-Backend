ffe6c52a25dbf432cfaa3062c7360b0098de13c2
NicklasFjeldsted <nicklasfjeldstedosbeck@gmail.com>
2024-11-05 16:35:12 +0100

    Logs

--- lib/PlanSphere.Core/Features/Companies/Commands/UploadCompanyLogo/UploadCompanyLogoCommandHandler.cs
+++ lib/PlanSphere.Core/Features/Companies/Commands/UploadCompanyLogo/UploadCompanyLogoCommandHandler.cs
@@ -16,13 +16,15 @@ public class UploadCompanyLogoCommandHandler(
     ILogger<UploadCompanyLogoCommandHandler> logger
 ) : IRequestHandler<UploadCompanyLogoCommand, string>
 {
-    
     private readonly ICompanyRepository _companyRepository = companyRepository ?? throw new ArgumentNullException(nameof(companyRepository));
     private readonly ILogger<UploadCompanyLogoCommandHandler> _logger = logger ?? throw new ArgumentNullException(nameof(logger));
     private readonly IPrivateAzureBlobStorage _privateAzureBlobStorage = privateAzureBlobStorage ?? throw new ArgumentNullException(nameof(privateAzureBlobStorage));
     
     public async Task<string> Handle(UploadCompanyLogoCommand request, CancellationToken cancellationToken)
     {
+        _logger.BeginScope("Uploading company logo for company with id: [{companyId}]", request.CompanyId);
+        
+        _logger.LogInformation("Generating path and trying to upload image to directory");
         var pathBuilder = new PathBuilder();
         var filePath = pathBuilder
             .AddOrganisationDirectory(request.OrganisationId)
@@ -35,9 +37,11 @@ public class UploadCompanyLogoCommandHandler(
             request.Image.CopyTo(ms);
             var fileBytes = ms.ToArray();
             await _privateAzureBlobStorage.SaveAsync(filePath, fileBytes);
+            _logger.LogInformation("Successfully saved file to directory: {filepath}", filePath);
         }
-
+        _logger.LogInformation("Saving logo file path on company with id: [{companyId}]", request.CompanyId);
         await _companyRepository.UploadLogo(request.CompanyId, filePath, cancellationToken);
+        _logger.LogInformation("Saved logo file path on company with id: [{companyId}]", request.CompanyId);
 
         return filePath;
     }
