20521d7594b18532398024cafe6fb45d245937a3
NicklasFjeldsted <nicklasfjeldstedosbeck@gmail.com>
2024-11-05 15:58:55 +0100

    Added container name

--- lib/PlanSphere.Core/Constants/FileStorageConstants.cs
+++ lib/PlanSphere.Core/Constants/FileStorageConstants.cs
@@ -2,6 +2,7 @@
 
 public static class FileStorageConstants
 {
+    public const string PublicContainerName = "public";
     public const string UserDirectory = "users";
     public const string ProfilePictureDirectory = "profile-picture";
     public const string LogoPath = "logo";
