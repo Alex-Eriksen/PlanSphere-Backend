21c48c24eb5cf537b41dbbffbb192d6c7acdc8b1
kasp643d <kas10@hotmail.dk>
2024-11-06 17:17:37 +0100

    DeleteDepartment added

---
+++ lib/PlanSphere.Core/Features/Departments/Commands/DeleteDepartment/DeleteDepartmentCommand.cs
@@ -0,0 +1,5 @@
+﻿using MediatR;
+
+namespace PlanSphere.Core.Features.Departments.Commands.DeleteDepartment;
+
+public record DeleteDepartmentCommand (ulong Id) : IRequest;
---
+++ lib/PlanSphere.Core/Features/Departments/Commands/DeleteDepartment/DeleteDepartmentCommandHandler.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+using PlanSphere.Core.Attributes;
+using PlanSphere.Core.Enums;
+using PlanSphere.Core.Interfaces.Repositories;
+
+namespace PlanSphere.Core.Features.Departments.Commands.DeleteDepartment;
+
+[HandlerType(HandlerType.SystemApi)]
+public class DeleteDepartmentCommandHandler(
+    IDepartmentRepository departmentRepository,
+    ILogger<DeleteDepartmentCommandHandler> logger
+) : IRequestHandler<DeleteDepartmentCommand>
+{
+    private readonly IDepartmentRepository _departmentRepository = departmentRepository ?? throw new ArgumentNullException(nameof(departmentRepository));
+    private readonly ILogger<DeleteDepartmentCommandHandler> _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+
+    public async Task Handle(DeleteDepartmentCommand request, CancellationToken cancellationToken)
+    {
+        _logger.BeginScope("Deleting Department");
+        _logger.LogInformation("Deleting Department with id [{id}]", request.Id);
+        await _departmentRepository.DeleteAsync(request.Id, cancellationToken);
+        _logger.LogInformation("Deleted Department with id [{id}]!", request.Id);
+    }
+}
