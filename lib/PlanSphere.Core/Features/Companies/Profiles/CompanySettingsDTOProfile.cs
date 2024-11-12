e4f6969b9950d94ded8516ffef9cc39572959eb4
Alexander Vestergaard Eriksen <alexanderv.eriksen@gmail.com>
2024-11-12 11:14:29 +0100

    Updated get company by id to support settings

---
+++ lib/PlanSphere.Core/Features/Companies/Profiles/CompanySettingsDTOProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Domain.Entities;
+using PlanSphere.Core.Features.Companies.DTOs;
+
+namespace PlanSphere.Core.Features.Companies.Profiles;
+
+public class CompanySettingsDTOProfile : Profile
+{
+    public CompanySettingsDTOProfile()
+    {
+        CreateMap<CompanySettings, CompanySettingsDTO>()
+            .ForMember(dest => dest.DefaultRole, opt => opt.MapFrom(src => src.DefaultRole))
+            .ForMember(dest => dest.DefaultWorkSchedule, opt => opt.MapFrom(src => src.DefaultWorkSchedule))
+            .ForMember(dest => dest.InheritDefaultWorkSchedule, opt => opt.MapFrom(src => src.InheritDefaultWorkSchedule));
+    }
+}
