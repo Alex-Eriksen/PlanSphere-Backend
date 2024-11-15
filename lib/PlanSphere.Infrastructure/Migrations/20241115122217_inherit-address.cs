c24a0efa43820c7b69076f37f493761f4fe2e0f0
kasp643d <kas10@hotmail.dk>
2024-11-15 13:32:10 +0100

    DataMigration

---
+++ lib/PlanSphere.Infrastructure/Migrations/20241115122217_inherit-address.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PlanSphere.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class inheritaddress : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "InheritAddress",
+                table: "Companies",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "InheritAddress",
+                table: "Companies");
+        }
+    }
+}
